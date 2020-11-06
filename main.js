6d3a183f9fd81eebc617f72eeaff411768159a15
Fabio Vettori <fabio_vettori@hotmail.it>
2020-11-06 18:26:51 +0100

    ex + bonus

--- main.js
+++ main.js
@@ -1,5 +1,11 @@
-$(document).ready(function{
-    // Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
-    // BONUS: Aggiungere un’immagine presa anch’essa da un data
+// Vue app
+var root = new Vue({
+    el: '#root',
+    data: {
+        message: 'Stampa a schermo',
+        bground: 'back'
+    },
+    methods: {
 
+    }
 });
